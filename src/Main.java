36b1b44d32319179309b504e4a815b211f29458c
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-21 14:13:26 +0300

    Ex2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Example 1");
         int year = 2021;
         defineLeapYear(year);
+        System.out.println("Example 2");
+        String clientOS = "Android";
+        int clientDeviceYear = 2015;
+        defineRequiredLink(clientOS, clientDeviceYear);
     }
     public static void defineLeapYear(int year){
         if (year >= 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
@@ -16,6 +22,27 @@ public class Main {
         else
         {
             System.out.println(year + " год не является високосным");
+        }
+    }
+    public static void defineRequiredLink(String clientOS, int clientDeviceYear){
+        int currentYear = LocalDate.now().getYear();
+        switch (clientOS) {
+            case "iOS":
+                if (clientDeviceYear >= currentYear) {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                    break;
+                } else
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                break;
+            case "Android":
+                if (clientDeviceYear >= currentYear) {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                    break;
+                }
+                else
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            default:
+                System.out.println("");
         }
     }
 }
