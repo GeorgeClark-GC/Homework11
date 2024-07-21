f93628ff572830f744819e162666724aad0993fe
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-21 13:55:59 +0300

    Ex1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Example 1");
+        int year = 2021;
+        defineLeapYear(year);
+    }
+    public static void defineLeapYear(int year){
+        if (year >= 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        {
+            System.out.println(year + " год является високосным");
+        }
+        else if (year < 1584)
+        {
+            System.out.println(year + " год находится на числовой оси до введения високосного года в 1584");
+        }
+        else
+        {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
