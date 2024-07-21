5df31ed9d2772eec102077a3cd713a8c544c516f
GeorgeClark-GC <georgeclark2349@gmail.com>
2024-07-21 14:23:52 +0300

    Ex3

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,22 @@ public class Main {
         String clientOS = "Android";
         int clientDeviceYear = 2015;
         defineRequiredLink(clientOS, clientDeviceYear);
+        System.out.println("Example 3");
+        int deliveryDistance = 95;
+        countingDays(deliveryDistance);
     }
-    public static void defineLeapYear(int year){
-        if (year >= 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
-        {
+
+    public static void defineLeapYear(int year) {
+        if (year >= 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год является високосным");
-        }
-        else if (year < 1584)
-        {
+        } else if (year < 1584) {
             System.out.println(year + " год находится на числовой оси до введения високосного года в 1584");
-        }
-        else
-        {
+        } else {
             System.out.println(year + " год не является високосным");
         }
     }
-    public static void defineRequiredLink(String clientOS, int clientDeviceYear){
+
+    public static void defineRequiredLink(String clientOS, int clientDeviceYear) {
         int currentYear = LocalDate.now().getYear();
         switch (clientOS) {
             case "iOS":
@@ -38,11 +38,24 @@ public class Main {
                 if (clientDeviceYear >= currentYear) {
                     System.out.println("Установите версию приложения для Android по ссылке");
                     break;
-                }
-                else
+                } else
                     System.out.println("Установите облегченную версию приложения для Android по ссылке");
             default:
-                System.out.println("");
+                System.out.print("");
         }
+    }
+
+    public static void countingDays(int deliveryDistance) {
+        int deliveryTime = 1;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: " + deliveryTime);
+        } else if (deliveryDistance <= 60) {
+            deliveryTime = deliveryTime + 1;
+            System.out.println("Потребуется дней: " + deliveryTime);
+        } else if (deliveryDistance <= 100) {
+            deliveryTime = deliveryTime + 2;
+            System.out.println("Потребуется дней: " + deliveryTime);
+        } else
+            System.out.println("Доставки нет");
     }
 }
